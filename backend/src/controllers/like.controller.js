c5f2b4b43ba92a25c806c998048dd9c330a3efcc
Ankit <rajankit26smp@gmail.com>
2025-03-31 16:59:37 +0530

    like and post controller done

---
+++ backend/src/controllers/like.controller.js
@@ -0,0 +1,81 @@
+import Like from '../models/like.schema.js'
+import Post from '../models/post.schema.js'
+import asyncHandler from '../service/asyncHandler.js'
+import customError from '../service/customError.js'
+
+export const likePost = asyncHandler(async(req, res) =>{
+    const {postId} = req.params
+    const userId = req.user._id
+
+    if(!mongoose.Types.ObjectId.isValid(postId)){
+        throw new customError('Invalid postId', 403)
+    }
+
+    const post = await Post.findById(postId)
+    if(!post){
+        throw new customError('Post not found', 400)
+    }
+
+    // Ensure user hasn't already like the post 
+    const isLiked = await Like.findOne({userId, postId})
+    if(isLiked){
+        throw new customError('Post already liked by the user', 400)
+    }
+
+    const like = await Like.create({userId, postId});
+    res.status(201).json({
+        success : true,
+        mesaage : 'Post liked successfully',
+        like
+    })
+})
+
+export const dislikePost = asyncHandler(async(req, res) => {
+    const {postId} = req.params
+    const userId = req.user._id
+
+    if(!mongoose.Types.ObjectId.isValid(postId)){
+        throw new customError('Invalid postId', 400)
+    }
+
+    const post = await Post.findById(postId)
+
+    if(!post){
+        throw new customError('Post does not exist', 400)
+    }
+
+    const postAlreadyLiked = await Like.findOne({userId, postId})
+    if(!postAlreadyLiked){
+        throw new customError("can't dislike the post as it is not liked by the user", 400)
+    }
+
+    await postAlreadyLiked.deleteOne()
+
+    res.status(201).json({
+        success : true,
+        message : 'Post disliked successfully!'
+    })
+})
+
+export const getLikes = asyncHandler(async(req, res) =>{
+    const {postId} = req.params
+
+    if(!mongoose.Types.ObjectId.isValid(postId)){
+        throw new customError('Invalid postId', 400)
+    }
+
+    const allLikes = await Like.find({ postId }).select('+userId')
+    const usersList = []
+    
+    for(let i = 0; i < allLikes.length; i++){
+        usersList.push(allLikes[i].userId)
+    }
+    let likeCount = usersList.length
+
+    res.status(200).json({
+        success : true, 
+        message : 'Fetched all likes successfully',
+        usersList,
+        likeCount
+    })
+})
---
+++ backend/src/controllers/post.controller.js
@@ -0,0 +1,110 @@
+import Post from '../models/post.schema.js'
+import asyncHandler from '../service/asyncHandler.js'
+import customError from '../service/customError.js'
+
+export const createPost = asyncHandler(async(req, res) => {
+    const { content } = req.body
+    const userId = req.user._id
+
+    if(!content || !Array.isArray(content) || content.length === 0){
+        throw new customError('Post content is required and should be an array', 400)
+    }
+    
+    const userPost = await Post.create({userId, content})
+    res.status(200).json({
+        success : true,
+        message : 'Post created successfully',
+        post : userPost
+    })
+})
+
+export const updatePost = asyncHandler(async(req, res) => {
+    const { content } = req.body
+    const postId = req.params
+    const userId = req.user._id
+
+    if(!mongoose.Types.ObjectId.isValid(postId)){
+        throw new customError('Invalid postId', 403)
+    }
+    const post = await Post.findById(postId)
+    if(!post){
+        throw new customError('Post not found', 400)
+    }
+    
+    if(post.userId.toString() != userId.toString()){
+        throw new customError('You can only update your own post,unauthorized access!', 400);
+    }
+
+    if(!content && !Array.isArray(content)){
+        throw new customError('Content should be an array',400)
+    }
+
+    if(content){
+        post.content = content;
+    }
+
+    await post.save()
+    res.status(201).json({
+        success : true,
+        message : 'Post updated successfully',
+        post
+    })
+})
+
+// get posts by a specific user
+export const getUserPost = asyncHandler(async(req, res) => {
+    const userId  = req.user._id || req.params.userIdx
+
+    const post = await Post.find({userId})
+    if(!post || post.length === 0){
+        throw new customError('Post not found for the specific user', 400)
+    }
+
+    res.status(200).json({
+        success : true,
+        message : 'User posts fetched successfully',
+        post
+    })
+})
+
+// Get a Specific Post
+export const getPost = asyncHandler(async(req, res) => {
+    const {postId} = req.params
+
+    if(!mongoose.Types.ObjectId.isValid(postId)){
+        throw new customError('Invalid postId', 404)
+    }
+    const post = await Post.findById(postId)
+    if(!post){
+        throw new customError('Post not found', 400)
+    }
+
+    res.status(200).json({
+        success : true,
+        message : 'Post fetched successfully',
+        post
+    })
+})
+
+export const deletePost = asyncHandler(async(req, res) => {
+    const { postId } = req.params
+    const userId = req.user._id
+
+    if(!mongoose.Types.ObjectId.isValid(postId)){
+        throw new customError('Invalid postId', 403)
+    }
+    const post = await Post.findById(postId)
+    if(!post){
+        throw new customError('Post not found', 400)
+    }
+
+    if(post.userId.toString() != userId.toString()){
+        throw new customError('You are not allowed to delete the post, unauthorised access', 400)
+    }
+
+    await Post.deleteOne();
+    res.status(200).json({
+        success : true,
+        message : 'Post deleted successfully'
+    })
+})
